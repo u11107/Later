0e83f1ac11bedb1ba3126f6acc8fabfc2ee30b18
Rinat <eenot84@yandex.ru>
2022-09-06 21:39:34 +0300

    commit

--- src/test/java/ru/practicum/user/UserControllerTest.java
+++ src/test/java/ru/practicum/user/UserControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//comment
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
     @Mock
